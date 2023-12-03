2086cb6997e43af1c7522f61fe548cc61fece050
AlexF23 <152330796+AlexF23@users.noreply.github.com>
2023-12-03 23:01:03 +0000

    hello class

--- src/minihwalexf/MiniHWAlexF.java
+++ src/minihwalexf/MiniHWAlexF.java
@@ -15,6 +15,7 @@ public class MiniHWAlexF {
      */
     public static void main(String[] args) {
         System.out.println("Hello WOrld!");
+        System.out.println("Hello Class?");
     }
     
 }
