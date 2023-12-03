6a7a7b25e7e627e1f1e1b9a80aaa01cbd2f2e51a
AlexF23 <152330796+AlexF23@users.noreply.github.com>
2023-12-03 23:09:46 +0000

    make, model, wheel, colour

---
+++ src/minihwalexf/Car.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package minihwalexf;
+
+/**
+ *
+ * @author Alexander
+ */
+public class Car {
+    String make;
+    String model;
+    int numWheel;
+    String colour;
+    
+}
